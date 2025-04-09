621c3b5171261a5f82bf052c76ddc57c0b5a5854
pioner921227 <alex.shumihin921227@gmail.com>
2025-04-09 21:34:21 +0200

    refactoring

--- src/expo-plugins/withAutoSkeleton.js
+++ src/expo-plugins/withAutoSkeleton.js
@@ -1,36 +1,3 @@
-// import {
-//   withPlugins,
-//   withInfoPlist,
-//   withAndroidManifest,
-// } from '@expo/config-plugins';
-
-const {
-  withPlugins,
-  withInfoPlist,
-  withAndroidManifest,
-} = require('@expo/config-plugins');
-
-// если нужно что-то дописать в Info.plist — можно здесь
-const withAutoSkeletonIOS = (config) => {
-  return withInfoPlist(config, (config) => {
-    // config.modResults — это сам Info.plist в JS-виде
-    // можно добавить, если нужно:
-    // config.modResults.MySetting = 'Enabled';
-    return config;
-  });
+module.exports = function withAutoSkeleton(config) {
+  return config;
 };
-
-// если нужно что-то дописать в AndroidManifest.xml — можно здесь
-const withAutoSkeletonAndroid = (config) => {
-  return withAndroidManifest(config, (config) => {
-    // config.modResults.manifest — это AndroidManifest в JS-объекте
-    // например, можно добавить permissions, metadata и т.п.
-    return config;
-  });
-};
-
-const withAutoSkeleton = (config) => {
-  return withPlugins(config, [withAutoSkeletonIOS, withAutoSkeletonAndroid]);
-};
-
-module.exports = withAutoSkeleton;
